d1556a638ddbdccb8553ce18a214d9dd2e329416
Ran Bar-Zik <ran.bar-zik@hpe.com>
2016-08-21 18:10:40 +0300

    Aligning code to eslint standard

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var config = require('./testbot-config.json'),
-  botInteraction;
+'use strict';
+
+var config = require('./testbot-config.json');
+var botInteraction;
 
 //TODO: In the future there will be more services, remove the true flag then.
-if(!!config.bot === 'slack' || true) {
+if (!!config.bot === 'slack' || true) {
   botInteraction = require('./lib/slackbot-interaction');
 }
 
